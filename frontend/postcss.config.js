96b957e5c88d77de8868a0399a88bdec9b43bb90
mojig27 <mojig27@users.noreply.github.com>
2025-01-26 19:56:38 +0330

    حل تضادها در فایل postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 // frontend/postcss.config.js
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
-=======
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
->>>>>>> 82711d787c2dd095690a7e7ff331308412ff0fa2
+};
